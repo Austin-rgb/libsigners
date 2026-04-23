bf1159f006a71077a5046e25d1816a2a6c64327d
Ochieng Austine <114088922+Austin-rgb@users.noreply.github.com>
2026-04-23 17:25:54 +0300

    Update lib.rs
    made Sign injectable by ferrumec di

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,23 @@ pub use common::Claims;
 pub use hs256::HS256Signer;
 pub use rs256::{RS256Signer, RS256Validator};
 pub use signer_core::{Sign, Validate};
+
+use ferrumec::di::{AsyncFromEnv, EnvError};
+
+impl AsyncFromEnv for Arc<dyn Sign> {
+    async fn from_env(ctx: &crate::di::EnvContext) -> Result<Self, crate::di::EnvError> {
+        let signer_type = ctx.get("sign.type")?;
+        match signer_type {
+            "hs256" => {
+                Ok(Arc::new(HS256Signer::new(ctx.get("sign.aud")?.to_owned())) as Arc<dy>
+            }
+            "rs256" => Ok(Arc::new(RS256Signer::new(
+                ctx.get("sign.private_key")?.to_string(),
+                ctx.get("sign.aud")?.to_string(),
+            )) as Arc<dyn Sign>),
+            _ => Err(EnvError::new(format!(
+                "Unsupported sign.type value: {signer_type}"
+            ))),
+        }
+    }
+}
