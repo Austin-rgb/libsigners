5d668e4debbb95c411ea2c99fe0463e581f2acdb
Ochieng Austine <114088922+Austin-rgb@users.noreply.github.com>
2026-04-23 17:29:42 +0300

    Update lib.rs
    made validate injectable

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,11 @@ pub use common::Claims;
 pub use hs256::HS256Signer;
 pub use rs256::{RS256Signer, RS256Validator};
 pub use signer_core::{Sign, Validate};
-
+use std::sync::Arc;
 use ferrumec::di::{AsyncFromEnv, EnvError};
 
 impl AsyncFromEnv for Arc<dyn Sign> {
-    async fn from_env(ctx: &crate::di::EnvContext) -> Result<Self, crate::di::EnvError> {
+    async fn from_env(ctx: &crate::di::EnvContext) -> Result<Self,EnvError> {
         let signer_type = ctx.get("sign.type")?;
         match signer_type {
             "hs256" => {
@@ -23,6 +23,25 @@ impl AsyncFromEnv for Arc<dyn Sign> {
             )) as Arc<dyn Sign>),
             _ => Err(EnvError::new(format!(
                 "Unsupported sign.type value: {signer_type}"
+            ))),
+        }
+    }
+}
+
+impl AsyncFromEnv for Arc<dyn Validate> {
+    async fn from_env(ctx: &crate::di::EnvContext) -> Result<Self,EnvError> {
+        let signer_type = ctx.get("validate.type")?;
+        match signer_type {
+            "hs256" => Ok(
+                Arc::new(HS256Signer::new(ctx.get("validate.aud")?.to_owned()))
+                    as Arc<dyn Validate>,
+            ),
+            "rs256" => Ok(Arc::new(RS256Validator::new(
+                ctx.get("validate.public_key")?.to_string(),
+                ctx.get("validate.aud")?.to_string(),
+            )) as Arc<dyn Validate>),
+            _ => Err(EnvError::new(format!(
+                "Unsupported validate.type value: {signer_type}"
             ))),
         }
     }
